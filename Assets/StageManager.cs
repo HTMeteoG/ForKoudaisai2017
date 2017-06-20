11beb55ea9744c405ad3bd6cad78ad2899cbd901
Hayato <ht2_gr@ybb.ne.jp>
2017-06-20 10:47:42 +0900

    Add StageManager

---
+++ Assets/StageManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace koudaigame2017
+{
+    public class StageManager : MonoBehaviour
+    {
+        /*
+         * どうしよう
+         * フィールドは2Dにする
+         * 
+         */
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
